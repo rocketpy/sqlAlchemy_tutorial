2409d60949bb47b20047a9b6e7991ae619a01660
rocketpy <dimitr.ushakov@gmail.com>
2020-07-26 23:05:46 +0300

    updated

--- main_file.py
+++ main_file.py
@@ -1,4 +1,6 @@
 import sqlalchemy
+from sqlalchemy import MetaData, Table, Column, String, Integer
+
 
 # version
 sqlalchemy.__version__ 
@@ -9,6 +11,14 @@ from sqlalchemy import create_engine
 
 engine = create_engine("sqlite:///some.db")
 # engine = create_engine('sqlite:///:memory:', echo=True)
+
+# creating a table
+metadata = MetaData()
+user_table = Table('user', metadata,
+                   Column('id', Integer, primary_key=True),
+                   Column('username', String(50)),
+                   Column('fullname', String(50))
+                   )
 
 # execute
 result = engine.execute("select emp_id, emp_name from " 
