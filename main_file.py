774125c5b1eaac671bb35440f286f36c0c90c23f
rocketpy <dimitr.ushakov@gmail.com>
2020-07-26 23:02:48 +0300

    updated

--- main_file.py
+++ main_file.py
@@ -7,4 +7,12 @@ sqlalchemy.__version__
 # connecting
 from sqlalchemy import create_engine
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine("sqlite:///some.db")
+# engine = create_engine('sqlite:///:memory:', echo=True)
+
+# execute
+result = engine.execute("select emp_id, emp_name from " 
+                        "employee where emp_id=:emp_id",
+                        emp_id=3)
+# query same like
+# select emp_id, emp_name from employee where emp_id=3;
