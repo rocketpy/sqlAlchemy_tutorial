58170aa0cdd87e2e2507f97536661da9c612fc90
rocketpy <dimitr.ushakov@gmail.com>
2020-11-12 18:45:02 +0200

    updated

--- sqlite3.py
+++ sqlite3.py
@@ -5,13 +5,16 @@ conn = sqlite3.connect('customers.db')  # connecting to db
 
 cur = conn.cursor()  # create a cursor
 
+#  Datatypes: INTEGER, TEXT, NULL, BLOB, REAL
+
 #  create a table
 cur.execute("""CREATE TABLE customers(first_name text,
                                       second_name text,
-                                      email text)
-            """)
+                                      email text
+                                      )          
+           """)
 
-#  Datatypes: INTEGER, TEXT, NULL, BLOB, REAL
+cur.execute("INSERT INTO customers VALUES ('John', 'Doe', 'jdoe@gmail.com')")   # insert data to table 'customers'
 
 conn.commit()  # commit 
 
