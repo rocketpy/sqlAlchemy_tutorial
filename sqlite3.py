89cbff90922d8dcf808cec31b64173734eeacb24
rocketpy <dimitr.ushakov@gmail.com>
2020-11-13 23:01:17 +0200

    updated

--- sqlite3.py
+++ sqlite3.py
@@ -16,6 +16,7 @@ cur.execute("""CREATE TABLE customers(first_name text,
 
 #  add new one customer
 cur.execute("INSERT INTO customers VALUES ('John', 'Doe', 'jdoe@gmail.com')")   # insert data to table 'customers'
+conn.commit()  # commit 
 
 #  adding a many customers
 customers_list = [('John', 'Doe', 'jdoe@gmail.com'),
@@ -23,7 +24,13 @@ customers_list = [('John', 'Doe', 'jdoe@gmail.com'),
                  ('Jill', 'Boe', 'jill_boegmail.com')
                  ]
 cur.executemany("INSERT INTO customers VALUES (?,?,?)", customers_list) 
-
 conn.commit()  # commit 
+
+# fetch (get) data from table
+cur.execute("SELECT * FROM customers")
+cur.fetchone()
+cur.fetchmany(5)
+cur.fetchall()
+# print(cur.fetchall())
 
 conn.close()  # close connection
