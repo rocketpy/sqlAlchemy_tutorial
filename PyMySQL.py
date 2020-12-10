4b74823d0cde49c6ed5257a31a3eeb23a73e46cc
rocketpy <dimitr.ushakov@gmail.com>
2020-12-10 17:51:21 +0200

    updated

--- PyMySQL.py
+++ PyMySQL.py
@@ -1,1 +1,16 @@
+#  pip install PyMySQL
 
+import pymysql
+
+# fetchAll
+con = pymysql.connect('localhost', 'user17', 
+    'secret_password', 'testdb')
+ 
+with con: 
+    cur = con.cursor()
+    cur.execute("SELECT * FROM cities")
+ 
+    rows = cur.fetchall()
+ 
+    for row in rows:
+        print("{0} {1} {2}".format(row[0], row[1], row[2]))
