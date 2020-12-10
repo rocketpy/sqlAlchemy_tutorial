6a74bc7e9391bf0bea413aad18ce90a1fa7b1d89
rocketpy <dimitr.ushakov@gmail.com>
2020-12-10 17:59:33 +0200

    updated

--- PyMySQL.py
+++ PyMySQL.py
@@ -11,15 +11,13 @@ con = pymysql.connect('localhost', 'user17',
 with con: 
     cur = con.cursor()
     cur.execute("SELECT * FROM cities")
- 
     rows = cur.fetchall()
     # row = cur.fetchone()    
- 
     for row in rows:
         print("{0} {1} {2}".format(row[0], row[1], row[2]))
         
         
-# get column names by cursor dict 
+# get data by cursor dict 
 con = pymysql.connect(host='localhost',
         user='user17',
         password='s$cret',
@@ -28,11 +26,22 @@ con = pymysql.connect(host='localhost',
         cursorclass=pymysql.cursors.DictCursor)
  
 with con:
- 
     cur = con.cursor()
     cur.execute("SELECT * FROM cities")
- 
     rows = cur.fetchall()
- 
     for row in rows:
         print(row["id"], row["name"])
+        
+        
+# get column names
+con = pymysql.connect('localhost', 'user17', 
+    's$cret', 'testdb')
+ 
+with con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM cities")
+    rows = cur.fetchall()
+    desc = cur.description
+    print("{0:>3} {1:>10}".format(desc[0][0], desc[1][0]))
+    for row in rows:    
+        print("{0:3} {1:>10}".format(row[0], row[2]))
