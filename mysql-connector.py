ea87ae1b080a860e07a2bf55b9be244ca5256696
rocketpy <dimitr.ushakov@gmail.com>
2021-01-12 19:43:54 +0200

    Update mysql-connector.py

--- mysql-connector.py
+++ mysql-connector.py
@@ -10,9 +10,11 @@ db_connection = mysql.connector.connect(host="hostname",
 print(db_connection)
 
 # Create new database  
-CREATE DATABASE "database_name"
+# CREATE DATABASE "database_name"
 
 import mysql.connector
+
+
 db_connection = mysql.connector.connect(host= "localhost",
                                         user= "root",
                                         passwd= "root"
@@ -30,3 +32,26 @@ db_cursor.execute("SHOW DATABASES")
 # print all databases
 for db in db_cursor:
 	print(db)
+	
+# create a simple table "student" with two columns	
+# CREATE  TABLE student (id INT, name VARCHAR(255))
+
+import mysql.connector
+
+
+db_connection = mysql.connector.connect(host="localhost",
+                                        user="root",
+                                        passwd="root",
+                                        database="my_first_db"
+				       )
+
+db_cursor = db_connection.cursor()
+
+# creating database table as student
+db_cursor.execute("CREATE TABLE student (id INT, name VARCHAR(255))")
+
+# for get a table
+db_cursor.execute("SHOW TABLES")
+for table in db_cursor:
+	print(table)
+
